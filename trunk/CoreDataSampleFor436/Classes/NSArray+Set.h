63a72656c6cd6e627aa042cf87082dacabd20b14
epeter <epeter@e33daeaf-d549-1767-46c6-abc4f89a9e9e>
2010-04-12 23:44:17 +0000

    link coredatasamplefor436 to coredatalibrary template code

--- trunk/CoreDataSampleFor436/Classes/NSArray+Set.h
+++
@@ -1,5 +0,0 @@
-//Code from Jeff LeMarche (More iPhone 3 Development - Dec 2009)
-
-@interface NSArray(Set)
-+ (id)arrayByOrderingSet:(NSSet *)set byKey:(NSString *)key ascending:(BOOL)ascending;
-@end
